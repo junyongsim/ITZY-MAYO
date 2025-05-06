234b42bf87cea0715c0fe94500e112ee6c798d12
freecatdog <freedom_life_@naver.com>
2025-05-07 00:22:59 +0900

    feat: 메모리 게임 화면 MemoryGameActivity 생성 및 연결

--- app/src/main/java/com/syu/itzy_mayo/FeedFragment.java
+++ app/src/main/java/com/syu/itzy_mayo/FeedFragment.java
@@ -1,5 +1,6 @@
 package com.syu.itzy_mayo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 public class FeedFragment extends Fragment {
@@ -14,7 +16,12 @@ public class FeedFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_feed, container, false);
-
+        // 🧠 Memory Game 카드 클릭 이벤트 추가
+        CardView cardMemoryGame = rootView.findViewById(R.id.card_memory_game);
+        cardMemoryGame.setOnClickListener(v -> {
+            Intent intent = new Intent(requireContext(), MemoryGameActivity.class);
+            startActivity(intent);
+        });
         return rootView;
     }
 }
