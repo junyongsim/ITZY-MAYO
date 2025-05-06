d455baa7c9e6ff7974042d1d63c1fc3242353262
freecatdog <freedom_life_@naver.com>
2025-05-07 00:55:56 +0900

    feat: 사과 게임 화면 AppleGameActivity.java 생성 및 연결 ComingSoon 화면 ComingSoonActivity.java 생성 및 연결 2048 게임 화면 Game2048Activity.java 생성 및 연결 (메모리 게임 화면 MemoryGameActivity 생성 및 연결과 동일하게 해놓음.)

--- app/src/main/java/com/syu/itzy_mayo/FeedFragment.java
+++ app/src/main/java/com/syu/itzy_mayo/FeedFragment.java
@@ -16,12 +16,35 @@ public class FeedFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_feed, container, false);
-        // 🧠 Memory Game 카드 클릭 이벤트 추가
+
+        // 🧠 Memory Game
         CardView cardMemoryGame = rootView.findViewById(R.id.card_memory_game);
         cardMemoryGame.setOnClickListener(v -> {
             Intent intent = new Intent(requireContext(), MemoryGameActivity.class);
             startActivity(intent);
         });
+
+        // 🍎 Apple Game
+        CardView cardAppleGame = rootView.findViewById(R.id.card_apple_game);
+        cardAppleGame.setOnClickListener(v -> {
+            Intent intent = new Intent(requireContext(), AppleGameActivity.class);
+            startActivity(intent);
+        });
+
+        // ⏳ Coming Soon
+        CardView cardEtcGame = rootView.findViewById(R.id.card_etc_game);
+        cardEtcGame.setOnClickListener(v -> {
+            Intent intent = new Intent(requireContext(), ComingSoonActivity.class);
+            startActivity(intent);
+        });
+
+        // 🔢 2048 Game
+        CardView card2048 = rootView.findViewById(R.id.card_2048);
+        card2048.setOnClickListener(v -> {
+            Intent intent = new Intent(requireContext(), Game2048Activity.class);
+            startActivity(intent);
+        });
+
         return rootView;
     }
 }
